8d8a0fd4cca50cb08352e2a70e65aa4523f83f37
lily0335 <laraibanwar00002@gmail.com>
2025-09-10 06:11:31 +0500

    commit

--- hrmis/src/api/axios.js
+++ hrmis/src/api/axios.js
@@ -1,28 +1,10 @@
-// import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "https://hrmis-api.devfamz.com/api",
-// });
-
-// // Add Authorization header automatically if token exists
-// API.interceptors.request.use(config => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default API;
-// src/api.js
-// src/api/axios.js
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://hrmis-api.devfamz.com/api",
 });
 
-// ✅ Automatically attach token with every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
