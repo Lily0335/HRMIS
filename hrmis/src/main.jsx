fcd4b29e7e59a6fe8e1f81bc3b8da6bbebe0b0b6
lily0335 <laraibanwar00002@gmail.com>
2025-09-14 03:05:17 +0500

    commit

--- hrmis/src/main.jsx
+++ hrmis/src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { StatsProvider } from './context/StatsContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StatsProvider>
     <App />
-    </StatsProvider>
   </StrictMode>,
 )
