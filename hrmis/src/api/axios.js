fe5c4128951ef720f4cf9d9a1c6feb27889cb0f9
lily0335 <laraibanwar00002@gmail.com>
2025-09-14 02:59:46 +0500

    commit

--- hrmis/src/api/axios.js
+++ hrmis/src/api/axios.js
@@ -1,46 +1,9 @@
 
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "https://hrmis-api.devfamz.com/api",
-// });
-
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default API;
-
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "https://hrmis-api.devfamz.com/api",
-// });
-
-// API.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token"); // ✅ Pick JWT
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`; // ✅ Attach it
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// export default API;
-// src/api/axios.js
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://hrmis-api.devfamz.com/api",
 });
-
-// Attach token before every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
